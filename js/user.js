e75b98e8a5c01c360ff3e920b3213011d7902089
vera <vera0228@sohu.com>
2015-04-21 17:46:50 +0800

    update

--- js/user.js
+++ js/user.js
@@ -15,20 +15,14 @@ var userModule = (function() {
         //页面初始化
         login: function(name, pwd) {
             globalModule.jsonp('user', 'login', {'User[name]':name, 'User[password]':pwd}, userModule.afterLogin);
-
-//            $("#login-form").on('submit', function(){
-//                var postData = globalModule.getJsonDataFromSeriaArr($(this).serializeArray());
-//                globalModule.jsonp('user', 'login', postData, userModule.afterLogin);
-//                return false;
-//            });
         },
         afterLogin: function(result) {
             if (result.error == 0) {
                 var user = result.msg;
-                CLIENTSTATUS.login = true;
-                CLIENTSTATUS.uid = user.user_id;
-                CLIENTSTATUS.name = user.name;
-                window.location.href = "index.html";
+                window.localStorage['user-id'] = user.user_id;
+                window.localStorage['user-name'] = user.name;
+//                window.localStorage['password'] = user.name;
+                window.location.href = 'index.html';
             } else {
 
             }
